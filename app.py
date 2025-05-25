38ee854dd6c73578a0b9e59827a7366b41efcd94
Flow69420 <sai.prakhya2008@gmail.com>
2025-05-25 15:41:52 +1000

    added database and example record

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 
-app = (Flask(__name__))
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+from models import db, User
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
     return 'Welcome to FitQuest!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True, nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(8), default='user', nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.username}, {self.email}, {self.role}, {self.id}>"
