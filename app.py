195060be5b11c044e5e0c1b8362855c240317e7c
Flow69420 <sai.prakhya2008@gmail.com>
2025-05-25 19:16:03 +1000

    added wtforms

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template, redirect, url_for, flash
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -11,9 +12,34 @@ db.init_app(app)
 
 migrate = Migrate(app, db)
 
+from forms import RegistrationForm, LoginForm
+
 # with app.app_context():
 #     db.create_all()
 
 @app.route('/')
 def home():
     return 'Welcome to FitQuest!'
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successful!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.password == form.password.data:
+            flash('Login successful!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login failed. Check your email and password.', 'danger')
+    return render_template('login.html', form=form)
--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 SQLALCHEMY_DATABASE_URI = 'sqlite:///fitquest.db'
+SECRET_KEY = 'your_secret_key_here'
